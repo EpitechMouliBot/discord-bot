b2358cbb44498fbf9fd341f390dead753934beab
Thomas <thomas.ott@epitech.eu>
2022-02-11 16:13:04 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from json.tool import main
+import time
+import discord
+import asyncio
+
+####################INITS_VARIABLE####################
+client = discord.Client()
+
+####################INITS_FUNCTIONS####################
+
+
+# #####################MAIN#################################
+
+def main():
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
