175cf4435414527e0cc6016113dbee6c3bdde9f4
redboard <ott.thomas68@gmail.com>
2022-02-24 15:05:39 +0100

    add two port for check relay

--- src/manage_relay.py
+++ src/manage_relay.py
@@ -3,6 +3,7 @@ import requests
 def check_connected_with_relay():
     try:
         requests.get("http://localhost:4634/epitest/me/2021")
+        requests.get("http://localhost:4635/epitest/me/2021")
         return (True)
     except:
         return (False)
